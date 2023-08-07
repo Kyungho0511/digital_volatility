20627ef99b0df8a89a9c02af2e2d3bc28f0490e6
Kyungho0511 <kleearchitecture@gmail.com>
2023-08-07 11:48:01 -0400

    Mapbox template added

---
+++ config.js
@@ -0,0 +1,3 @@
+const config = {
+  MAPBOX_ACCESS_TOKEN : 'pk.eyJ1Ijoia2xlZTA1MTEiLCJhIjoiY2xrYnFibnNjMGV4cjNrbzRqdGg1d21sYiJ9.nN0pE1qocGhTLnD_xPuYdg'
+}
---
+++ main.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = config.MAPBOX_ACCESS_TOKEN;
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/klee0511/cll0zpgsj00s001qg066o7ljz',
+    zoom: 14,
+    center: [126.98957897888896, 37.54035898378337]
+});
+
